8b10e9ffba6e3d5aa112b507174fa62983f3ef30
Kay Putra <kaspr.works@gmail.com>
2022-05-22 00:31:15 +1000

    completed coding challenge 2

--- js-udemy-1/js-fundamentals-part-1/script.js
+++ js-udemy-1/js-fundamentals-part-1/script.js
@@ -1,12 +1,21 @@
-console.log("js fundamentals part 1 coding challenge 1");
+console.log("js fundamentals part 1 coding challenge 2");
 
 const markWeight = 78;
 const markHeight = 1.69;
 const johnWeight = 92;
 const johnHeight = 1.95;
-const markBMI = markWeight / (markHeight * markHeight);
-const johnBMI = johnWeight / (johnHeight * johnHeight);
+const markBMI = Math.round(markWeight / (markHeight * markHeight));
+const johnBMI = Math.round(johnWeight / (johnHeight * johnHeight));
 const markHigherBMI = markBMI > johnBMI;
-console.log(`Mark's BMI ${Math.round(markBMI)}`);
-console.log(`John's BMI ${Math.round(johnBMI)}`);
-console.log(`Is Mark's BMI is higher than John's: ${markHigherBMI}`);
+// const markHigherBMI =
+//   markBMI > johnBMI
+//     ? `Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`
+//     : `John's BMI (${johnBMI}) is higher than Marks's (${markBMI})`;
+// console.log(`Mark's BMI ${markBMI}`);
+// console.log(`John's BMI ${johnBMI}`);
+// console.log(`Is Mark's BMI is higher than John's: ${markHigherBMI}`);
+// console.log(markHigherBMI);
+
+if (markHigherBMI)
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+else console.log(`John's BMI (${johnBMI}) is higher than Marks's (${markBMI})`);
