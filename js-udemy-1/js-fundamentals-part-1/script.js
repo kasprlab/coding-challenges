0ee1ec288082dfcc1ec43a32841a5c8270b5ac59
Kay Putra <kaspr.works@gmail.com>
2022-05-21 23:55:10 +1000

    completed coding challenge 1

--- js-udemy-1/js-fundamentals-part-1/script.js
+++ js-udemy-1/js-fundamentals-part-1/script.js
@@ -1,1 +1,12 @@
-console.log("coding challenges");
+console.log("js fundamentals part 1 coding challenge 1");
+
+const markWeight = 78;
+const markHeight = 1.69;
+const johnWeight = 92;
+const johnHeight = 1.95;
+const markBMI = markWeight / (markHeight * markHeight);
+const johnBMI = johnWeight / (johnHeight * johnHeight);
+const markHigherBMI = markBMI > johnBMI;
+console.log(`Mark's BMI ${Math.round(markBMI)}`);
+console.log(`John's BMI ${Math.round(johnBMI)}`);
+console.log(`Is Mark's BMI is higher than John's: ${markHigherBMI}`);
